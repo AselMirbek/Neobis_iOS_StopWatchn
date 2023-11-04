49afbca8c27f93eb7015cc6dc0710a04cec82dbe
Asel <mirbekovaasel663@gmail.com>
2023-11-04 16:41:31 +0600

    Initial Commit
    Проблемы с инициализацией

--- Neobis_iOS_StopWatchn/ViewController.swift
+++ Neobis_iOS_StopWatchn/ViewController.swift
@@ -9,7 +9,7 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    
+    //kj
     
     @IBOutlet weak var timerLabel: UILabel!
     @IBOutlet weak var stopButton: UIButton!
@@ -27,8 +27,11 @@ class ViewController: UIViewController {
     private var elapsedTime: TimeInterval = 0
     var remainingTime: TimeInterval = 0
     
-    
-    
+    private var timerElapsedTime: TimeInterval = 0 // Для таймера
+    var timerRemainingTime: TimeInterval = 0 // Для таймера
+
+    private var stopwatchElapsedTime: TimeInterval = 0 // Для секундомера
+   
     let hoursData = Array(0...23)
     let minutesData = Array(0...59)
     let secondsData = Array(0...59)
@@ -50,68 +53,72 @@ class ViewController: UIViewController {
     @IBAction func segmentedControlValueChanged(_ sender: UISegmentedControl) {
         updateImage()
      
-        if sender.selectedSegmentIndex == 1 {
-            timerPicker.isHidden = true}
-        else   {
-            timerPicker.isHidden = false
-        }
+        if sender.selectedSegmentIndex == 0 {
+                timerPicker.isHidden = true
+            }
+            else   {
+                timerPicker.isHidden = false
+                elapsedTime = 0
+            }
         
     }
     
     @IBAction func startButtonTapped(_ sender: UIButton) {
         if isRunning {
-              return
-          }
-          isRunning = true
-          startButton.isEnabled = false
-          pauseButton.isEnabled = true
-          stopButton.isEnabled = true
-          timerPicker.isHidden = true // Скрыть UIPickerView
+               return
+           }
+           isRunning = true
+           startButton.isEnabled = false
+           pauseButton.isEnabled = true
+           stopButton.isEnabled = true
+        timerPicker.isHidden = true
 
-          if segmentedController.selectedSegmentIndex == 0 {
-              // Timer
-              let selectedTime = selectedHours * 3600 + selectedMinutes * 60 + selectedSeconds
-              remainingTime = TimeInterval(selectedTime)
-              startTimer()
-          } else {
-              // Stopwatch
-              if timer == nil {
-                  timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(updateTime), userInfo: nil, repeats: true)
-                  RunLoop.current.add(timer!, forMode: .common)
-              } else {
-                  // Resume the timer
-                  timer?.fire()
-              }
+           if segmentedController.selectedSegmentIndex == 0 {
+           
+               let selectedTime = selectedHours * 3600 + selectedMinutes * 60 + selectedSeconds
+               remainingTime = TimeInterval(selectedTime)
+              // updateTime()
+               startTimer()
+           } else {
+             
               
-              startTime = Date().timeIntervalSinceReferenceDate - elapsedTime
-          }
-              
+               if timer == nil {
+                   timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(updateTime), userInfo: nil, repeats: true)
+                   RunLoop.current.add(timer!, forMode: .common)
+               } else {
+                   timer?.fire()
+               }
+               startTime = Date().timeIntervalSinceReferenceDate - elapsedTime
+               // Обнулить elapsedTime для секундомера
+               elapsedTime = 0
+           }
+   
     }
     
+    
+    
     @IBAction func stopButtonTapped(_ sender: UIButton) {
-        if segmentedController.selectedSegmentIndex == 0 {
-               // Timer segment selected
-               startButton.isEnabled = true
-               pauseButton.isEnabled = false
-               stopButton.isEnabled = false
-               if let timer = timer {
-                   timer.invalidate()
-               }
-               isRunning = false
-               elapsedTime = 0 // Сбросить elapsed time к нулю
-               timerLabel.text = "00:00:00" // Сбросить timerLabel к "00:00:00"
-           } else {
-               // Stopwatch segment selected
-               startButton.isEnabled = true
-               pauseButton.isEnabled = false
-               stopButton.isEnabled = false
-               if let timer = timer {
-                   timer.invalidate()
-               }
-               isRunning = false
-               updateTime()
+        startButton.isEnabled = true
+           pauseButton.isEnabled = false
+           stopButton.isEnabled = false
+        timerPicker.isHidden = false // Показать UIPickerView
+           if let timer = timer {
+               timer.invalidate()
            }
-        
+           isRunning = false
+
+           if segmentedController.selectedSegmentIndex == 0 {
+             
+               remainingTime = 0 // Обнулить remainingTime для таймера
+           } else {
+             
+               // Обновить label и продолжить отсчет времени
+              updateTime()
+              
+           }
+
+           // timerPicker.isHidden = false // Показать UIPickerView
+       
     }
     
     @IBAction func pauseButtonTapped(_ sender: UIButton) {
@@ -127,26 +134,28 @@ class ViewController: UIViewController {
     }
     
     @objc private func updateTime() {
-        if segmentedController.selectedSegmentIndex == 1 {
-                // Stopwatch segment selected
+        if segmentedController.selectedSegmentIndex == 0 {
+                // Таймер сегмент (index 0)
+                if remainingTime <= 0 {
+                    stopTimer()
+                    return
+                }
+                remainingTime -= 1
+                let hours = Int(remainingTime) / 3600
+                let minutes = (Int(remainingTime) % 3600) / 60
+                let seconds = Int(remainingTime) % 60
+                timerLabel.text = String(format: "%02d:%02d:%02d", hours, minutes, seconds)
+            } else if segmentedController.selectedSegmentIndex == 1 {
+                // Секундомер сегмент (index 1)
                 elapsedTime = Date().timeIntervalSinceReferenceDate - startTime
                 let hours = Int(elapsedTime) / 3600
                 let minutes = (Int(elapsedTime) % 3600) / 60
                 let seconds = Int(elapsedTime) % 60
                 timerLabel.text = String(format: "%02d:%02d:%02d", hours, minutes, seconds)
-            } else {
-                // Timer segment selected
-                if remainingTime <= 0 {
-                    stopTimer()
-                    return
-                }
-            remainingTime -= 1
-            let hours = Int(remainingTime) / 3600
-            let minutes = (Int(remainingTime) % 3600) / 60
-            let seconds = Int(remainingTime) % 60
-            timerLabel.text = String(format: "%02d:%02d:%02d", hours, minutes, seconds)
-        }
-    }
+            }}
+        
+    
+
 
     func startTimer() {
         timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(updateTime), userInfo: nil, repeats: true)
@@ -177,18 +186,18 @@ class ViewController: UIViewController {
     }
     func updateTimerLabel() {
         if segmentedController.selectedSegmentIndex == 0 {
-            // Stopwatch segment selected
-            let totalSeconds = selectedHours * 3600 + selectedMinutes * 60 + selectedSeconds
-            elapsedTime = TimeInterval(totalSeconds-1) // Отнимите 1, чтобы начать с 0
-            updateTime()
+               
+                let hours = Int(remainingTime) / 3600
+                let minutes = (Int(remainingTime) % 3600) / 60
+                let seconds = Int(remainingTime) % 60
+                timerLabel.text = String(format: "%02d:%02d:%02d", hours, minutes, seconds)
+            } else {
+              
+                let hours = selectedHours
+                let minutes = selectedMinutes
+                let seconds = selectedSeconds
+                timerLabel.text = String(format: "%02d:%02d:%02d", hours, minutes, seconds)
             }
-        else{
-            // Timer segment selected
-            let hours = selectedHours
-                       let minutes = selectedMinutes
-                       let seconds = selectedSeconds
-                       timerLabel.text = String(format: "%02d:%02d:%02d", hours, minutes, seconds)
-        }
     }
 }
     // MARK: UIPickerView DataSource and Delegate Methods
@@ -228,11 +237,12 @@ class ViewController: UIViewController {
             selectedMinutes = minutesData[pickerView.selectedRow(inComponent: 1)]
             selectedSeconds = secondsData[pickerView.selectedRow(inComponent: 2)]
             updateTimerLabel()
-             if segmentedController.selectedSegmentIndex == 1 {
-             // Stopwatch segment selected
-             let totalSeconds = selectedHours * 3600 + selectedMinutes * 60 + selectedSeconds
-             elapsedTime = TimeInterval(totalSeconds - 1) // чтобы начать с 0
-             updateTime()}
+            if segmentedController.selectedSegmentIndex == 1 {
+                
+                let totalSeconds = selectedHours * 3600 + selectedMinutes * 60 + selectedSeconds
+                elapsedTime = TimeInterval(totalSeconds - 1) // чтобы начать с 0
+               updateTime()
+            }
             }
         }
         
